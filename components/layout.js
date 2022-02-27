0ee91b998bffa8f9390f22d77e9375cc39e2a59e
n-huzaifa <noorhuzaifa11@gmail.com>
2022-02-27 12:19:50 +0500

    test

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Noor ul islam huzaifa'
+const name = 'preview test'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
